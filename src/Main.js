0d41f4172435058d49b4b57a12c03eb1b84558ff
Coop <64296432+Coop8@users.noreply.github.com>
2023-02-15 16:57:06 -0500

    updated cname position, package json, and some ui elements

--- src/Main.js
+++ src/Main.js
@@ -16,55 +16,55 @@ function Main() {
   };
   
   return (
-        <header className="Hero">
-          <div className="Profile-logo">
-            <img src={logo} width="100" height="50" alt="logo"/>
-          </div>
-          <p>
-            Cooper Rockwell
-          </p>
-          <div className="Horizontal">
-            <p>
-              Computer engineering student at&nbsp;
-            </p>
-            <a
-              className="University"
-              href="https://www.purdue.edu/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Purdue University
-            </a>
-          </div>
+    <header className="Hero">
+      <div className="Profile-logo">
+        <img src={logo} width="100" height="50" alt="logo"/>
+      </div>
+      <h2>
+        Cooper Rockwell
+      </h2>
+      <div className="Horizontal">
+        <p>
+          Computer engineering student at&nbsp;
           <a
-            className="Web-link"
-            href="https://github.com/Coop8"
+            className="University"
+            href="https://www.purdue.edu/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Github
+            Purdue University
           </a>
-          <a
-            className="Web-link"
-            href="https://www.linkedin.com/in/cooperrockwell/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            LinkedIn
-          </a>
-          <h3>Projects</h3>
-          <div id="cards">
-            <div className="card">
-              <div className="card-content"></div>
-            </div>
-            <div className="card">
-              <div className="card-content"></div>
-            </div>
-            <div className="card">
-              <div className="card-content"></div>
-            </div>
-          </div>
-        </header>
+        </p>
+      </div>
+      <a
+        className="Web-link"
+        href="https://github.com/Coop8"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Github
+      </a>
+      <a
+        className="Web-link"
+        href="https://www.linkedin.com/in/cooperrockwell/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        LinkedIn
+      </a>
+      <h3>Projects</h3>
+      <div id="cards">
+        <div className="card">
+          <div className="card-content"></div>
+        </div>
+        <div className="card">
+          <div className="card-content"></div>
+        </div>
+        <div className="card">
+          <div className="card-content"></div>
+        </div>
+      </div>
+    </header>
   );
 }
 
